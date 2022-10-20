d69474c911ce5377203226c926f5b5fcc67dc75f
Mahmoud Sayed <108075016+MahmoudSayedA@users.noreply.github.com>
2022-10-20 18:55:02 +0200

    Delete vehicleManager.java

--- implementation/vehicleManager.java
+++
@@ -1,7 +0,0 @@
-public class vehicleManager {
-    public static boolean IsItHisVehicle(Vehicle vehicle, VehicleSpec vSpec, IDriver driver) {
-        return (vehicle.getSpec().match(vSpec) &&
-                vehicle.getVehicleDriver().match(driver));
-    }
-}
-//second sprint
